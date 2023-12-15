addfa1ed3b1e0fe7e27cce99afdc855cb3b4461f
Hugo-Novo-Gouvea <novogouvea@gmail.com>
2023-12-15 17:59:53 -0300

    att

--- jogos/urls.py
+++ jogos/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from jogos.views import index, jogo, certificados, certificado, buscar, buscarCertificado
 
 urlpatterns = [ 
-    path('', jogo, name='jogo'),
-    #path('jogo/<int:jogo_id>', jogo, name='jogo'),
+    path('', index, name='index'),
+    path('jogo/<int:jogo_id>', jogo, name='jogo'),
     path('certificados', certificados, name='certificados'),
     path('ceritifcado/<int:certificado_id>', certificado, name='certificado'),
     path("buscar", buscar, name="buscar"),
-    path("buscarCertificado", buscarCertificado, name="buscar"),
+    path("buscarCertificado", buscarCertificado, name="buscarCertificado"),
 ]
