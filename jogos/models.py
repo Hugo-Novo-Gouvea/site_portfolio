2725c67ffdf8df40e1e3ac51c8e6e58b3a8146d6
Hugo-Novo-Gouvea <novogouvea@gmail.com>
2023-12-16 17:47:31 -0300

    finalizacao html

--- jogos/models.py
+++ jogos/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from datetime import datetime
 
 OPCOES_ENGINE = [
-        ("UNITY", "Unity"),
-        ("UNREAL 5", "Unreal 5"),
+        ("Unity", "Unity"),
+        ("Unreal 5", "Unreal 5"),
     ]
 
 class Jogos(models.Model):
@@ -12,8 +12,14 @@ class Jogos(models.Model):
     legenda = models.CharField(max_length = 150, null = False, blank = False)
     engine = models.CharField(max_length = 100,choices = OPCOES_ENGINE, default='')
     descricao = models.TextField(null = False, blank = False)
-    imagem = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    imagem_capa = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    imagem_demo_1 = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    imagem_demo_2 = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    imagem_demo_3 = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    imagem_demo_4 = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default = False)
+    nome_jam = models.CharField(max_length = 150, null = True, blank = True)
+    link_jam = models.CharField(max_length = 150, null = True, blank = True)
     data_jogo = models.DateTimeField(default = datetime.now, blank = False)
 
     def __str__(self):
